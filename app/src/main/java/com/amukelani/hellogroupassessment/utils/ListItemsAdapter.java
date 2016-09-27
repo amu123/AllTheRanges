b87268473c47f298c1f8bd50ff2d52b1e3ef786e
amu123 <amukelanigezanifreedommathye@gmail.com>
2016-09-27 08:45:06 +0100

    Update ListItemsAdapter.java

--- app/src/main/java/com/amukelani/hellogroupassessment/utils/ListItemsAdapter.java
+++ app/src/main/java/com/amukelani/hellogroupassessment/utils/ListItemsAdapter.java
@@ -1,4 +1,4 @@
-package com.raywenderlich.hellogroupassessment.utils;
+package com.amukelani.hellogroupassessment.utils;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
